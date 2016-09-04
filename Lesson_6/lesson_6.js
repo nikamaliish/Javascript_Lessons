62612ae2f09dff69b443c8ade10f310f1ade400a
Nikamaliish <nikamaliish@mail.ru>
2016-09-04 16:02:15 +0300

    Update the functions;
    Added 2 versions in the functions with list: with loop and with recursion

--- Lesson_6/lesson_6.js
+++ Lesson_6/lesson_6.js
@@ -1,142 +1,210 @@
-/**
- * Created by User on 03.07.2016.
- */
-//Задание 1.
-//Объекты могут быть использованы для построения различных структур данных. Часто
-//встречающаяся структура – список (не путайте с массивом). Список – связанный набор
-//объектов, где первый объект содержит ссылку на второй, второй – на третий, и т.п.
-//    var list = { value: 1, rest: { value: 2, rest: {value: 3, rest: null } } };
-//Списки удобны тем, что они могут делиться частью своей структуры. Например, можно сделать два
-//списка, {value: 0, rest: list} и {value: -1, rest: list}, где list – это ссылка на ранее объявленную
-//переменную. Это два независимых списка, при этом у них есть общая структура list, которая включает
-// три последних элемента каждого из них. Кроме того, оригинальный список также сохраняет свои
-//свойства как отдельный список из трёх элементов. Напишите функцию arrayToList, которая строит
-//такую структуру, получая в качестве аргумента [1, 2, 3], а также функцию listToArray, которая создаёт
-//массив из списка. Также напишите вспомогательную функцию prepend, которая получает элемент и
-//создаёт новый список, где этот элемент добавлен спереди к первоначальному списку, и функцию nth,
-//которая в качестве аргументов принимает список и число, а возвращает элемент на заданной
-//позиции в списке, или же undefined в случае отсутствия такого элемента.
+//Напишите функцию arrayToList, которая строит такую структуру, получая в качестве аргумента [1, 2, 3],
+
+var list = {value: 1, rest: {value: 2, rest: {value: 3, rest: null}}};
 
 function arrayToList(arr) {
-
     var rest = null;
-
     for (var i = arr.length - 1; i >= 0; i--) {
         rest = {value: arr[i], rest: rest};
     }
     return rest;
 }
 
+console.log(arrayToList([10, 20, 50, 5, 90, 667]));
 
-function listToArray(list) { // черезчур заморочено получилось, кажется. Можно ведь сделать проще?
-    var arr = [];
-    listToArray1(list, arr);
-
-    function listToArray1(list, arr) {
-        arr.push(list.value);
-        if (list.rest) {
-            listToArray1(list.rest, arr);
-        }
-        return arr;
+function arrayToList2(arr) {
+    var rest;
+    if (arr.length == 0) {
+        rest = null;
+    } else {
+        rest = {value: arr.shift(), rest: arrayToList2(arr)};
     }
 
-    return arr;
+    return rest;
 }
 
+console.log(arrayToList2([10, 20, 50, 5, 90, 667]));
+
+// а также функцию listToArray, которая создаёт массив из списка.
+
+function listToArray(rest) {
+    var valuesOfList = [];
+    while (rest) {
+        valuesOfList.push(rest.value);
+        rest = rest.rest;
+    }
+    return valuesOfList;
+}
+
+console.log(listToArray({value: 10, rest: {value: 20, rest: {value: 30, rest: null}}})); // → [10, 20, 30]
+
+function listToArray2(list) {
+    var valuesOfList = [];
+
+    function restToArray(rest, arr) {
+        if (rest !== null) {
+            arr.push(rest.value);
+            restToArray(rest.rest, arr);
+        }
+    }
+
+    restToArray(list, valuesOfList);
+
+    return valuesOfList;
+}
+
+console.log(listToArray2({value: 10, rest: {value: 20, rest: {value: 30, rest: null}}})); // → [10, 20, 30]
+
+//Также напишите вспомогательную функцию prepend, которая получает элемент и
+//создаёт новый список, где этот элемент добавлен спереди к первоначальному списку, и функцию nth,
+//которая в качестве аргументов принимает список и число, а возвращает элемент на заданной
+//позиции в списке, или же undefined в случае отсутствия такого элемента.
 
 function prepend(element, list) {
-    var newList = {value: element, rest: list};
-    return newList;
+    return {value: element, rest: list};
 }
 
-
-function nth(list, num) {
-    if (!list) {
-        return undefined;
-    }
-    else if (num === 0) {
-        return list.value;
-    }
-    else {
-        return nth(list.rest, num - 1);
-    }
-}
-
-
-var list = arrayToList([1, 2, 3, 4, 5]);
-console.log(list);
-
-list = prepend(10, list);
-console.log(list);
-
-console.log(nth(list, 4));
-console.log(nth(list, 7));
-
-var arr = listToArray(list);
-console.log(arr);
-
-
-console.log(arrayToList([10, 20]));
-console.log(listToArray({value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}));
 console.log(prepend(10, prepend(20, null)));
-console.log(nth(arrayToList([10, 20, 30]), 1));
+// → {value: 10, rest: {value: 20, rest: null}}
 
 
-//Задание 2.
+function nth(rest, num) {
+    var valuesOfList = [];
+
+    while (rest) {
+        valuesOfList.push(rest.value);
+        rest = rest.rest;
+    }
+
+    return valuesOfList[num] || undefined;
+}
+
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 20
+
+
 //Напишите две функции, reverseArray и reverseArrayInPlace. Первая получает массив как
-//аргумент и выдаёт новый массив, с обратным порядком элементов. Вторая работает как
-//оригинальный метод reverse – она меняет порядок элементов на обратный в том массиве,
-//который был ей передан в качестве аргумента. Не используйте стандартный метод reverse.
+//аргумент и выдаёт новый массив, с обратным порядком элементов.
 
 function reverseArray(arr) {
+    var reverseArr = [];
 
-    var newArr = [];
     for (var i = arr.length - 1; i >= 0; i--) {
-        newArr.push(arr[i]);
+        reverseArr.push(arr[i]);
     }
-    return newArr;
+    return reverseArr;
 }
 
+console.log(reverseArray([10, 20, 50, 5, 90, 667]));
+
+//Вторая работает как оригинальный метод reverse – она меняет порядок элементов на обратный в том массиве,
+//который был ей передан в качестве аргумента. Не используйте стандартный метод reverse.
 
 function reverseArrayInPlace(arr) {
 
     for (var i = arr.length - 1; i >= 0; i--) {
-        var n;
-        n = arr.splice(i - arr.length, 1);
-        arr.push(n[0]);
+        arr.push(arr.splice(i, 1)[0]);
     }
-    return arr;
 }
 
-console.log(reverseArray(arr));
-console.log(reverseArrayInPlace(arr));
+var arr = [10, 20, 50, 5, 90, 667];
+reverseArrayInPlace(arr);
+console.log(arr);
 
-//Задание 3.
+
 //Реализовать функцию pick(obj, keys), которая принимает аргументами объект и массив строк
 //(названия ключей). Возвращает новый объект, куда вошли все ключи, указанные в
 //массиве keys, и соответствующие значения из объекта obj. Если в объекте obj, нет ключа,
 //указанного в массиве keys, в результирующем объекте этот ключ не должен присутствовать.
 
-
 function pick(obj, keys) {
-    var newObj = {};
-    for (var key in obj) {
-        if (keys.indexOf(key) >= 0) {
-            newObj[key] = obj[key];
+    var pickedObj = {},
+        properties = Object.keys(obj);
+
+    for (var i = 0; i < properties.length; i++) {
+        if (~keys.indexOf(properties[i])) {
+            pickedObj[properties[i]] = obj[properties[i]];
         }
     }
-    return newObj;
+    return pickedObj;
 }
 
-
 var user = {
-        name: 'Sergey',
-        age: 30,
-        email: 'sergey@gmail.com',
-        friends: ['Sveta', 'Artem']
-    },
-    params = ['name', 'email', 'friends'];
+    name: 'Sergey',
+    age: 30,
+    email: 'sergey@gmail.com',
+    friends: ['Sveta', 'Artem']
+};
 
-console.log(pick(user, params));
-console.log(pick(user, ['name']));
+console.log(pick(user, ['friends']));
 
+
+//Задание 4.
+//Создайте класс Person. Объявите приватные свойства (в js нет приватных свойств, но вы можете
+// их имитировать с помощью замыкания) name и age. Добавьте методы setAttribute и getAttribute
+// - первый должен устанавливать приватное свойство, например, setAttribute('name', 'Вася'), а
+// второй соответственно получать - getAttribute('name'). Создайте несколько экземпляров класса.
+
+function Person() {
+    var attribute = {};
+
+    this.getAttribute = function (key) {
+        return attribute[key];
+    };
+
+    this.setAttribute = function(key, value){
+      attribute[key] = value;
+    }
+}
+
+var petya = new Person();
+petya.setAttribute('name', 'Vasya');
+console.log(petya.getAttribute('name'));
+console.log(petya.name);
+
+petya.name = 'Petya';
+console.log(petya.name);
+console.log(petya.getAttribute('name'));
+console.log(petya.name);
+console.log(petya.getAttribute('name'));
+
+//Задание 5.
+//В ходе 6 урока мы с вами написали аналог встроенного метода bind, но он имел урезанный функционал,
+// мы с помощью него могли фиксировать только контекст, но не параметры. Расширьте нашу функцию bind
+// такой возможностью, реализуйте в ней фиксацию параметров.
+
+
+function bind() {
+    var args = [].slice.call(arguments),
+        f = args.shift(),
+        context = args.shift();
+
+    return function () {
+        f.apply(context, args.concat([].slice.call(arguments)));
+    };
+}
+
+var binded = bind(function (param1, param2, param3) {
+    console.log(param1, param2, param3);
+}, {}, 11, 13);
+
+binded(2, 3, 4, 6);
+
+
+Function.prototype.myBind = function () {
+    var args = [].slice.call(arguments),
+        f = this,
+        context = args.shift();
+
+    return function () {
+        f.apply(context, args.concat([].slice.call(arguments)));
+    };
+};
+
+function test(param1, param2, param3) {
+    console.log(param1, param2, param3);
+}
+
+var binded2 = test.myBind({}, 11, 12);
+
+binded2(4, 6);
