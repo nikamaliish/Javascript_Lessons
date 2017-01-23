54ed9e4c6620696dbba5446a4bea2c38af2f12f9
Nikamaliish <nikamaliish@mail.ru>
2017-01-23 23:53:50 +0300

    +comment

--- Concrete_Margematics/Chapter1/Tower_of_Hanoi.js
+++ Concrete_Margematics/Chapter1/Tower_of_Hanoi.js
@@ -21,11 +21,11 @@ function getNumberOfMoves_Cycle(numberOfDiscs) {
     return numberOfMoves;
 }
 
-// i (numberOfDiscs) = 1, numberOfMoves = 2*0 + 1 = 1;
-// i (numberOfDiscs) = 2, numberOfMoves = 2*1 + 1 = 3;
-// i (numberOfDiscs) = 3, numberOfMoves = 2*3 + 1 = 7;
-// i (numberOfDiscs) = 4, numberOfMoves = 2*7 + 1 = 15;
-// i (numberOfDiscs) = 5, numberOfMoves = 2*15 + 1 = 31;
-// i (numberOfDiscs) = 6, numberOfMoves = 2*31 + 1 = 62;
-// i (numberOfDiscs) = 7, numberOfMoves = 2*62 + 1 = 127;
-// i (numberOfDiscs) = 8, numberOfMoves = 2*127 + 1 = 255;
+// i = 1, numberOfMoves = 2*0 + 1 = 1;
+// i = 2, numberOfMoves = 2*1 + 1 = 3;
+// i = 3, numberOfMoves = 2*3 + 1 = 7;
+// i = 4, numberOfMoves = 2*7 + 1 = 15;
+// i = 5, numberOfMoves = 2*15 + 1 = 31;
+// i = 6, numberOfMoves = 2*31 + 1 = 62;
+// i = 7, numberOfMoves = 2*62 + 1 = 127;
+// i = 8, numberOfMoves = 2*127 + 1 = 255;
