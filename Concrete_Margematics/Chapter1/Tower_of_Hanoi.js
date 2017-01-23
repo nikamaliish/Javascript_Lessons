67730f34012c4edb2dc50a21dedac2420079134f
Nikamaliish <nikamaliish@mail.ru>
2017-01-23 23:13:07 +0300

    + comment

--- Concrete_Margematics/Chapter1/Tower_of_Hanoi.js
+++ Concrete_Margematics/Chapter1/Tower_of_Hanoi.js
@@ -20,3 +20,12 @@ function getNumberOfMoves_Cycle(numberOfDiscs) {
 
     return numberOfMoves;
 }
+
+// i (numberOfDiscs) = 1, numberOfMoves = 2*0 + 1 = 1;
+// i (numberOfDiscs) = 2, numberOfMoves = 2*1 + 1 = 3;
+// i (numberOfDiscs) = 3, numberOfMoves = 2*3 + 1 = 7;
+// i (numberOfDiscs) = 4, numberOfMoves = 2*7 + 1 = 15;
+// i (numberOfDiscs) = 5, numberOfMoves = 2*1 + 1 = 31;
+// i (numberOfDiscs) = 6, numberOfMoves = 2*15 + 1 = 62;
+// i (numberOfDiscs) = 7, numberOfMoves = 2*15 + 1 = 127;
+// i (numberOfDiscs) = 8, numberOfMoves = 2*15 + 1 = 255;
